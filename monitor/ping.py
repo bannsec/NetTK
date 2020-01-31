1ffcc8db5256402599f14a5f3b9288dfee883199
IOhannes m zmölnig <zmoelnig@umlautS.umlaeute.mur.at>
2020-01-31 22:07:39 +0100

    changed shebang to python3
    using /usr/bin/env

--- monitor/ping.py
+++ monitor/ping.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!/usr/bin/env python3 -u
 from scapy.all import *
 from time import sleep
 
--- netTK.py
+++ netTK.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python -u
+#!/usr/bin/env python3 -u
 
 __builtins__.VERSION = "0.1"
 
