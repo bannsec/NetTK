7591c87f1c137fc8bdb3aae3ecab20f18e6c84d9
bannsec <7632864+bannsec@users.noreply.github.com>
2024-08-30 23:08:49 -0400

    Refactor `nettk/netTK.py` to use a `main` function
    * Move the existing script logic into the `main` function
    * Update the script to call the `main` function if the script is run directly

--- nettk/netTK.py
+++ nettk/netTK.py
@@ -46,43 +46,47 @@ dispatcher = {
 	'ping': ping
 }
 
-# Register our SIGINT handler
-signal.signal(signal.SIGINT, signal_handler)
+def main():
+	# Register our SIGINT handler
+	signal.signal(signal.SIGINT, signal_handler)
 
-# Define an addRecord queue
-addRecord = Queue()
+	# Define an addRecord queue
+	addRecord = Queue()
 
-# Define our exit event to allow threads to cleanly exit
-shouldExit = threading.Event()
+	# Define our exit event to allow threads to cleanly exit
+	shouldExit = threading.Event()
 
-# Open up our config file
-config = ConfigParser()
+	# Open up our config file
+	config = ConfigParser()
 
-# Read the config
-config.read(CONFIGFILE)
+	# Read the config
+	config.read(CONFIGFILE)
 
-# Loop through the config, starting up whatever we need to.
-for section in config._sections:
-	# Generate the tag for the module to use
-	kargs = dict(config._sections[section])
-	kargs["tag"] = kargs["module"].lower()
-	if kargs.get("ctag"):
-		kargs["tag"] += "_" + kargs["ctag"]
+	# Loop through the config, starting up whatever we need to.
+	for section in config._sections:
+		# Generate the tag for the module to use
+		kargs = dict(config._sections[section])
+		kargs["tag"] = kargs["module"].lower()
+		if kargs.get("ctag"):
+			kargs["tag"] += "_" + kargs["ctag"]
 
-	# Add addRecord and shouldExit to kargs
-	kargs["addRecord"] = addRecord
-	kargs["shouldExit"] = shouldExit
+		# Add addRecord and shouldExit to kargs
+		kargs["addRecord"] = addRecord
+		kargs["shouldExit"] = shouldExit
 
-	# Generic thread call. Looks up the "test" paramter (case insensitive) in dispatcher to know what to call.
-	t = threading.Thread(target=dispatcher[config._sections[section]["module"].lower()], kwargs=kargs)
-	t.start()
+		# Generic thread call. Looks up the "test" paramter (case insensitive) in dispatcher to know what to call.
+		t = threading.Thread(target=dispatcher[config._sections[section]["module"].lower()], kwargs=kargs)
+		t.start()
 
-# Wait for the user to want to exit
-print("Press Enter To Exit\n")
-try:
-	raw_input()
-except NameError:
-	input()
+	# Wait for the user to want to exit
+	print("Press Enter To Exit\n")
+	try:
+		raw_input()
+	except NameError:
+		input()
 
-# Let the threads know we should be exiting
-shouldExit.set()
+	# Let the threads know we should be exiting
+	shouldExit.set()
+
+if __name__ == "__main__":
+	main()
